6407d161ad8fe85dc80635f712b62bfff88d5f47
KingGnomo115 <kevinjec115@gmail.com>
2020-11-23 23:49:08 -0600

    Cree el método Modificacion para ccambiar datos en la base de datos

--- src/basico/ABCM.java
+++ src/basico/ABCM.java
@@ -41,7 +41,7 @@ public class ABCM
             ManipulaBD.desconecta(con);
             System.out.println("Dato insertado");
         }
-        
+
         //Bajas
         Connection con2 = ManipulaBD.conecta();
         if (con2 != null)
@@ -50,15 +50,15 @@ public class ABCM
             sql.Delete(con2, "EjemploTabla", "item2", "'" + item22 + "'");
         }
         ManipulaBD.desconecta(con2);
-        
+
         //Consultas
         Connection con3 = ManipulaBD.conecta();
         if (con3 != null)
         {
             Querys sql = new Querys();
-            ArrayList<Objeto> ap = ManipulaBD.cargarO(sql.Seleccion(con3, "*", "EjemploTabla", "id="+3+""));
+            ArrayList<Objeto> ap = ManipulaBD.cargarO(sql.Seleccion(con3, "*", "EjemploTabla", "id=" + 1 + ""));
             ManipulaBD.desconecta(con3);
-            if (ap!=null)
+            if (ap != null)
             {
                 System.out.println(ap.get(0).desp());
             } else
@@ -68,6 +68,16 @@ public class ABCM
         }
 
         //Modificaciones
+        Connection con4 = ManipulaBD.conecta();
+        if (con4 != null)
+        {
+            Querys sql = new Querys();
+            ArrayList<Objeto> ap = ManipulaBD.cargarO(sql.Seleccion(con4, "*", "EjemploTabla", "id=" + 3 + ""));
+            if (ap != null)
+            {
+                sql.Modificar(con4, "EjemploTabla", "id", "2", "id=3");
+            }
+        }
     }
     
 }
